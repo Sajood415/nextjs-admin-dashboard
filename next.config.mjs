49f06e6016b43e16c8fa7c465d7f93687f287ce4
Sajood Ur Rehman <sajood415@gmail.com>
2024-05-23 03:22:07 +0500

    fix: fix home page redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    images: {
-        domains: ["cdn-icons-png.flaticon.com", "robohash.org", "cdn.dummyjson.com", "via.placeholder.com"],
-      },    
-  };
-  
-  export default nextConfig;
-  
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      "cdn-icons-png.flaticon.com", 
+      "robohash.org", 
+      "cdn.dummyjson.com", 
+      "via.placeholder.com"
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
